625934516bb659cb907d935325dd411ed96f4960
raoul2000 <manu34@free.fr>
2016-08-17 20:32:38 +0200

    clean dependencies and starts config mount

--- config/params.php
+++ config/params.php
@@ -11,5 +11,48 @@ return [
     'filePattern' => "*.jpg",
     // timezone adjustment applied to the file last modification date
     // timezone support in php : http://php.net/manual/fr/timezones.php
-    'timezone' => "Europe/Paris"
+    'timezone' => "Europe/Paris",
+    'fs' => [
+      'basePath' => '@runtime/sample-data',
+      'baseUrl'  => 'http://localhost/devws/lab/cam-browser2/runtime/sample-data',
+      'mount' => [
+          'mount1' => [
+            'mount-point' => '/',
+            'fstype' => 'local',
+            'baseUrl'  => 'http://localhost/devws/lab/cam-browser2/runtime/sample-data',  // optionnal
+            'options' => [  // see http://flysystem.thephpleague.com/adapter/local/
+              'rootPath' => '/a/b/c',
+              'links' => "skip", // 'skip', 'disallow',
+              'permissions' => [
+                'file' => [
+                  'public' => 0744,
+                  'private' => 0700
+                ],
+                'dir' => [
+                  'public' => 0755,
+                  'private' => 0700
+                ]
+              ]
+            ]
+          ],
+          'mount2' => [
+            'mount-point' => '/folder',
+            'fstype' => 'ftp',
+            'baseUrl'  => 'http://hostname/folder1',  // optionnal
+            'options' => [
+              'host' => 'ftp.example.com',
+              'username' => 'username',
+              'password' => 'password',
+          
+              /** optional config settings */
+              'port' => 21,
+              'root' => '/path/to/root',
+              'passive' => true,
+              'ssl' => true,
+              'timeout' => 30,
+            ]
+
+          ]
+      ]
+    ]
 ];
